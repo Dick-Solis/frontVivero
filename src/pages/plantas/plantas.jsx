d59f00670d571a7015ac423b2ddf79a809537baf
Dick Solis <dicksolisromero@gmail.com>
2023-04-23 15:37:19 -0500

    Create component search

--- src/pages/plantas/plantas.jsx
+++ src/pages/plantas/plantas.jsx
@@ -1,13 +1,50 @@
+import styled from "styled-components";
+import {AiOutlineSearch} from "react-icons/ai";
+
+//#region Style
+  const ContainerPagePlantas = styled.main`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  `;
+
+  const ContainerSections = styled.section`
+    display: flex;
+    justify-content: center;
+    /* background-color:red; */
+    gap: 5px;
+    width: 100%;
+    padding: 0 20px;
+  `;
+
+  const StyledInput = styled.input`
+    width: 700px;
+    outline: none;
+    border: 2px solid #085b71;
+    border-radius: 5px;
+    padding: 0 10px;
+    font-size: 18px;
+  `;
+
+  const StyledButton = styled.button`
+    border: none;
+    background-color: #075d7cba;
+    border-radius: 5px;
+  `;
+
+
+//#endregion Style
+
+
 export function Plantas(){
   return(
-    <>
-      <h2>Plantas</h2>
-    <h2>Plantas</h2>
-
-    <h2>Plantas</h2>
-    <h2>Plantas</h2>
-    <h2>Plantas</h2>
-    </>
-    
+    <ContainerPagePlantas>
+      <ContainerSections>
+        <StyledInput type="text" />
+        <StyledButton>
+          <AiOutlineSearch style={{color:'white',fontSize:"27px",fontWeight: "bold"}}/>
+        </StyledButton>
+      </ContainerSections>
+    </ContainerPagePlantas>
   )
 }
